25da0a501be32b5bfdba0450af1556fefb9a266a
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-03-20 22:26:21 +0300

    Variables and Data Types sections have been finished.

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log(`Hello World!`);
+let userEmail = `mustafa.akdemir24@gmail.com`;
+let age = 45;
+let name = `Mustafa`;
+
+age = 46;
+
+// console.log(age);
+
+const points = 100;
+
+// console.log(points);
+
+// Currency Converter
+
+// $AUD to $USD Formula:
+
+// AUD = USD * 1.5
+
+let USD = 1200;
+let AUD = USD * 1.5;
+
+console.log(AUD);
