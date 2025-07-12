cd3d3cdff41d9ee6a975c14072dbb212cdb43897
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-07-12 12:47:12 +0300

    For loop chapter exercise

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-// For Loop
-
-for(let i=0; i < 5; i++) {
-  console.log(i);
-  
+for (let i = 1; i <= 15; i++) {
+  if(i % 5 === 0) {
+    console.log(`${i} - ASAP Frontend`); 
+  } else if (i % 2 === 1) {
+    console.log(`${i} - ASAP`);
+    
+  } else if (i % 2 === 0) {
+    console.log(`${i} - Frontend`);    
+  }
 }
