c0015dbf61df68f6c0538f47861043ea539fae1c
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-07-22 15:20:11 +0300

    For loop
    For loop lesson has been completed.

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-for (let i = 1; i <= 15; i++) {
-  if(i % 5 === 0) {
-    console.log(`${i} - ASAP Frontend`); 
-  } else if (i % 2 === 1) {
-    console.log(`${i} - ASAP`);
-    
-  } else if (i % 2 === 0) {
-    console.log(`${i} - Frontend`);    
-  }
+str = `ASAP Frontend`;
+
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i]);
 }
