6b4cb46698798e5342f12c447c6c7325eb7b22be
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-07-24 00:08:40 +0300

    Array methods
    Filter method lesson has been completed.

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-let array = [20, 15, 10, 30];
+let ageGroup1 = [18, 20, 16, 15, 21];
+let ageGroup2 = [16, 17, 14, 15, 19];
+let ageGroup3 = [20, 22, 26, 16, 28];
 
-// Array methods
-let filteredArray = array.filter((element) => {
-  return element <= 15;
-});
+// const filteredAgeGroup = (ageGroup) => {
+//   return ageGroup.filter((age) => age >= 18);
+// };
 
-console.log(filteredArray);
+// console.log(`Filtered Age Group 1: ${filteredAgeGroup(ageGroup1)}`);
+// console.log(`Filtered Age Group 2: ${filteredAgeGroup(ageGroup2)}`);
+// console.log(`Filtered Age Group 3: ${filteredAgeGroup(ageGroup3)}`);
+
+// console.log(ageGroup1.filter((age) => age >= 18));
+// console.log(ageGroup2.filter((age) => age >= 18));
+// console.log(ageGroup3.filter((age) => age >= 18));
+
+const addAdults = (ageGroup) => {
+  const adults = Array();
+  for (let i = 0; i < ageGroup.length; i++) {
+    if (ageGroup[i] >= 18) {
+      adults.push(ageGroup[i]);
+    }
+  }
+  return adults;
+};
+
+const group1 = addAdults(ageGroup1);
+const group2 = addAdults(ageGroup2);
+const group3 = addAdults(ageGroup3);
+
+console.log(group1);
+console.log(group2);
+console.log(group3);
+
+console.log(`Adults from Age Group 1: ${group1}`);
