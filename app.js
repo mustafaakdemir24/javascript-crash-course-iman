8a4926116f526302557b6482ccaddc66eda78277
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-07-25 14:56:58 +0300

    Array functions
    Map function exercise

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-usDollars = [10, 20, 30, 40];
+usDollars1 = [10, 20, 30, 40];
+usDollars2 = [100, 150, 35];
 
-const auDollars = usDollars.map((usDollar) => usDollar * 1.5);
+const auDollars1 = usDollars1.map((usDollar) => usDollar * 1.5);
+const auDollars2 = usDollars2.map((usDollar) => usDollar * 1.5);
 
-console.log(auDollars);
+console.log(auDollars1);
+console.log(auDollars2);
