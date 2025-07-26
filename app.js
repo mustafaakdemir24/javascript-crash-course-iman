b7f5bca80df0114f6963f68527eccc54a26628e4
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-07-26 13:37:07 +0300

    Array functions has been completed

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ const auDollars2 = usDollars2.map((usDollar) => usDollar * 1.5);
 
 console.log(auDollars1);
 console.log(auDollars2);
+
+const convertToAuDollars = (usDollar) => {
+  const auDollar = Array();
+  for (let i = 0; i < usDollar.length; i++) {
+    auDollar[i] = usDollar[i] * 1.5;
+  }
+
+  return auDollar;
+};
+
+const auDollar3 = convertToAuDollars(usDollars1);
+const auDollar4 = convertToAuDollars(usDollars2);
+
+console.log(auDollar3);
+console.log(auDollar4);
