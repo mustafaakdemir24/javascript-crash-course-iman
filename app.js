4a37c7f61460e7d5b80f960e3f8e2b960088cbf6
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-07-29 20:06:54 +0300

    JavaScript DOM

--- app.js
+++ app.js
@@ -1,39 +1,5 @@
-let users = [
-  {
-    email: `example@gmail.com`,
-    password: `test1234`,
-    name: `John Doe`,
-    discord: `examplediscord`,
-    subscription: `VPI+`,
-    lessonCompleted: [1, 2, 3],
-  },
-  {
-    email: `inamuser@gmail.com`,
-    password: `test1234`,
-    name: `Iman Musa`,
-    discord: `imancodes`,
-    subscription: `VPI+`,
-    lessonCompleted: [1, 2, 3, 4, 5],
-  },
-];
+// 1st method
+console.log(document.querySelector(`.heading`));
 
-console.log(users[0].email);
-
-console.log(users[0].name[0]);
-
-console.log(users[0].lessonCompleted.map((lesson) => lesson * 3));
-
-const addUser = (user) => {
-  users.push(user);
-};
-
-addUser({
-  email: `mustafa.akdemir24@gmail.com`,
-  password: `Tes1234`,
-  name: `Mustafa Akdemir`,
-  discord: `mustafaakdemir24`,
-  subscription: `VPI+`,
-  lessonCompleted: [1, 2, 3, 4],
-});
-
-console.log(users[2].lessonCompleted);
+// 2nd method
+console.log(document.getElementsByClassName("heading"));
